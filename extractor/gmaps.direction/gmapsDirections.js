11204ce5db29031430a701e11bb9a69d322f52f0
David Alcobero <david.alcobero@gmail.com>
2016-03-14 01:14:20 +0000

    Update

--- extractor/gmaps.direction/gmapsDirections.js
+++
@@ -1,6 +0,0 @@
-var gm = require('googlemaps');
-var util = require('util');
-
-gm.config('key', 'AIzaSyBXxGUQW5W2OLFCOEbb-vdFXFWaZ-XzzOs');
-gm.directions('51.515675951,-0.089294426', '51.513436264,-0.137885325' , 
-function(err, data){util.puts(JSON.stringify(data));});
